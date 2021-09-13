670e64d4aeaa042006f36dadeaaa7ecb55130685
1179991386 <1179991386@qq.com>
2021-09-13 20:14:25 +0800

    vis初始化代码

--- vue.config.js
+++ vue.config.js
@@ -1,10 +1,10 @@
 console.log(process.env.NODE_ENV, '=====process.env.NODE_ENV==')
-// module.exports = {
-//     publicPath: process.env.NODE_ENV === 'production'
-//       ? '/vis-web/'
-//       : '/'
-//   }
-
 module.exports = {
-    publicPath: './'
-}
+    publicPath: process.env.NODE_ENV === 'production'
+      ? '/vis-web/'
+      : '/'
+  }
+
+// module.exports = {
+//     publicPath: './'
+// }
